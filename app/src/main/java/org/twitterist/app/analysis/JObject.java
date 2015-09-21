bd53520faa5955a942aae28644840ac77bead569
Marco Wüthrich <marco.w.93@hotmail.ch>
2015-09-21 16:37:33 +0200

    Add JSON Parser
    JSON Parser and JObjects Klass

---
+++ app/src/main/java/org/twitterist/app/analysis/JObject.java
@@ -0,0 +1,29 @@
+package org.twitterist.app.analysis;
+
+/**
+ * Created by marco.wuethrich on 21.09.2015.
+ */
+public class JObject {
+
+    private String status = null;
+    private Double result = 0D;
+
+    public JObject() {
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Double getResult() {
+        return result;
+    }
+
+    public void setResult(Double result) {
+        this.result = result;
+    }
+}
